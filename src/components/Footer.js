802f9ddd29861e2ec435a7a29b7581a545d60c9e
Tiago Martinho <t.martinho@icloud.com>
2018-12-21 20:55:04 +0000

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,9 @@ const Footer = (props) => (
             <ul className="icons">
                 <li><a href="https://twitter.com/gigeconomyhow" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
             </ul>
+            <ul className="copyright">
+            <li>&copy; All rights reserved</li><li>GigEconomy.How</li><li><a href="https://html5up.net">Legal Information</a></li>
+            </ul>
         </div>
     </footer>
 )
